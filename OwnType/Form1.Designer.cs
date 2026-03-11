fbfe9b73612401db57f108cc1b793c93c08fed30
m1nca0 <noname890256@gmail.com>
2026-03-11 08:43:09 +0800

    create form

--- OwnType/Form1.Designer.cs
+++ OwnType/Form1.Designer.cs
@@ -28,12 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            textBox7 = new TextBox();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 6);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(106, 6);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(164, 6);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 2;
+            label3.Text = "label3";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(48, 24);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(52, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(106, 24);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(52, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(164, 24);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(52, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(48, 53);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(52, 23);
+            textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(106, 53);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(52, 23);
+            textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(164, 53);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(52, 23);
+            textBox6.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(4, 27);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 9;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(4, 56);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 10;
+            label5.Text = "label5";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(48, 82);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(168, 23);
+            textBox7.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "+", "-", "*", "&", "len a", "len b" });
+            comboBox1.Location = new Point(222, 38);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(48, 23);
+            comboBox1.TabIndex = 12;
+            comboBox1.Text = "+";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(275, 115);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox7);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox7;
+        private ComboBox comboBox1;
     }
 }
